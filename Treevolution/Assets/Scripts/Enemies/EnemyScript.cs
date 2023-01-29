5f1aa095213adcece4133bb3cfb0e2ee812f2b60
oscar <lj20886@bristol.ac.uk>
2023-01-29 15:33:41 +0000

    EnemyScript uses Pathfinder to move Currently the enemy moves so fast that you can't see it moving. Feel free to make changes

--- Treevolution/Assets/Scripts/Enemies/EnemyScript.cs
+++ Treevolution/Assets/Scripts/Enemies/EnemyScript.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Rigidbody enemyRigidbody;
     // Start is called before the first frame update
     void Start()
     {
+        Pathfinder.UpdatePathfindingGraph();
+        Vector3 treePosition = GameObject.FindGameObjectWithTag("Tree").transform.position;
+        Vector3[] pathToTree = Pathfinder.GetPath(transform.position, treePosition);
+        moveEnemy(pathToTree);
+    }
 
+    private void moveEnemy(Vector3[] path)
+    {
+        foreach (Vector3 node in path)
+        {
+            enemyRigidbody.MovePosition(node);
+        }
     }
 
     // Update is called once per frame
