c563945c97c1dd0fd1daa85098e388171b359d39
oscar <lj20886@bristol.ac.uk>
2023-01-30 12:43:50 +0000

    2nd Attempt moving enemy - moves visibly, although slows down before reaching a node (we probably want the speed to be constant)

--- Treevolution/Assets/Scripts/Enemies/EnemyScript.cs
+++ Treevolution/Assets/Scripts/Enemies/EnemyScript.cs
@@ -6,27 +6,36 @@ using Pathfinding;
 public class EnemyScript : MonoBehaviour
 {
     public Rigidbody enemyRigidbody;
+    private List<Vector3> path;
+    private Vector3 lastPosition;
+    private int counter = 0;
+    private bool moving = false;
+
     // Start is called before the first frame update
     void Start()
     {
         Pathfinder.UpdatePathfindingGraph();
         Vector3 treePosition = GameObject.FindGameObjectWithTag("Tree").transform.position;
-        Vector3[] pathToTree = Pathfinder.GetPath(transform.position, treePosition);
-        moveEnemy(pathToTree);
+        path = new List<Vector3>(Pathfinder.GetPath(transform.position, treePosition));
+        lastPosition = gameObject.transform.position;
     }
 
-    private void moveEnemy(Vector3[] path)
+
+    // Update is called once per physics frame
+    void FixedUpdate()
     {
-        foreach (Vector3 node in path)
+        if (counter < path.Count && !moving && (path[counter] - gameObject.transform.position).magnitude > 0.01f)
         {
-            enemyRigidbody.MovePosition(node);
+            enemyRigidbody.MovePosition(gameObject.transform.position + (path[counter] - gameObject.transform.position) * Time.deltaTime);
+            Debug.Log("Moving to: " + path[counter].ToString());
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        else if ((counter < path.Count) && (path[counter] - enemyRigidbody.position).magnitude <= 0.01f)
+        {
+            lastPosition = path[counter];
+            counter+=1;
+            Debug.Log(counter);
+            moving = false;
+        }
     }
 
     public void Damage()
