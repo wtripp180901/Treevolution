59c7e3dd421311f4294583f9764e60eaee6ed7c4
Will Tripp <will.tripp@btinternet.com>
2023-01-30 13:43:49 +0000

    Events test

--- Treevolution/Assets/Scripts/QR/QRDetection.cs
+++ Treevolution/Assets/Scripts/QR/QRDetection.cs
@@ -3,35 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.QR;
 using TMPro;
+using Microsoft.MixedReality.OpenXR;
 
 public class QRDetection : MonoBehaviour
 {
     QRCodeWatcher watcher;
     TMP_Text debugText;
+    GameObject debugMarker;
     
     async void Start()
     {
+        debugMarker = GameObject.FindGameObjectWithTag("Player");
         Debug.Log("QR Script started");
         debugText = GameObject.FindGameObjectWithTag("DebugText").GetComponent<TMP_Text>();
         debugText.text = "tst";
         await QRCodeWatcher.RequestAccessAsync();
         Debug.Log(QRCodeWatcher.IsSupported());
-        watcher = new QRCodeWatcher();
-        watcher.Start();
+        if (QRCodeWatcher.IsSupported())
+        {
+            watcher = new QRCodeWatcher();
+            watcher.Added += codeAddedEventHandler;
+            watcher.Updated += codeUpdatedEventHandler;
+            watcher.Start();
+            debugText.text = "started "+watcher.GetList().Count;
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        debugText.text = "updt_code";
+        //debugText.text = "updt_code";
         if (QRCodeWatcher.IsSupported())
         {
-            debugText.text = "S_" + watcher.GetList().Count.ToString() + "\n_" + watcher.GetList()[0].Data;
+            /*Pose position;
+            SpatialGraphNode.FromDynamicNodeId(watcher.GetList()[0].SpatialGraphNodeId).TryLocate(FrameTime.OnUpdate, out position);
+            debugMarker.transform.position = position.position;
+            debugText.text = "S_" + watcher.GetList().Count.ToString() + "\n_" + watcher.GetList()[0].Data + "\n_"+position.position.ToString();*/
         }
         else
         {
             debugText.text = "U";
         }
+    }
+
+    void codeUpdatedEventHandler(object sender,QRCodeUpdatedEventArgs args)
+    {
+        /*Pose position;
+        SpatialGraphNode.FromDynamicNodeId(args.Code.SpatialGraphNodeId).TryLocate(FrameTime.OnUpdate, out position);
+        debugMarker.transform.position = position.position;*/
+        //debugText.text = "S_\n_" + args.Code.Data + "\n_" + position.position.ToString();
+        debugText.text = "updated";
+    }
+
+    void codeAddedEventHandler(object sender, QRCodeAddedEventArgs args)
+    {
+        /*Pose position;
+        SpatialGraphNode.FromDynamicNodeId(args.Code.SpatialGraphNodeId).TryLocate(FrameTime.OnUpdate, out position);
+        debugMarker.transform.position = position.position;*/
+        //debugText.text = "S_\n_" + args.Code.Data + "\n_" + position.position.ToString();
+        debugText.text = "added";
     }
 }
