d672306105c93a933a76abde6b96567c525d26eb
Will Tripp <will.tripp@btinternet.com>
2023-01-31 19:29:56 +0000

    Added skeleton code

---
+++ Treevolution/Assets/Scripts/Towers/TowerGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TowerGenerator
+{
+    //Randomly generates a list of towers of length numberOfTowers, each with the points variable split randomly between their stats
+    public static List<TowerData> GenerateTowers(int points,int numberOfTowers)
+    {
+        //TODO: Implement
+        return new List<TowerData>() { new TowerData(),new TowerData()};
+    }
+}
