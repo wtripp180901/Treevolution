a7ff0b9f2d47579731f7fe49bed6556c24ec3df0
xl20294 <xl20294@bristol.ac.uk>
2023-02-01 12:39:10 +0000

    Enemy spawner at random position script complete

--- Treevolution/Assets/Scripts/Control/EnemyManager.cs
+++ Treevolution/Assets/Scripts/Control/EnemyManager.cs
@@ -6,6 +6,12 @@ public class EnemyManager : MonoBehaviour
 {
     private List<EnemyScript> _enemies = new List<EnemyScript>();
     public EnemyScript[] enemies { get { return _enemies.ToArray(); } }
+
+    public GameObject cubePrefab;
+    public float timer = 0;
+    public float spawnRate = 1;
+    public float coordinate_X = -5;
+    public float coordinate_Y = 5;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +22,16 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer < spawnRate)
+            timer = timer + Time.deltaTime;
+        else {
+            spawnEnemy();
+            timer = 0;
+        }
+    }
+
+    void spawnEnemy(){
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(coordinate_X, coordinate_Y), 1, Random.Range(coordinate_X, coordinate_Y));
+        Instantiate(cubePrefab, randomSpawnPosition, transform.rotation);
     }
 }
