090599d35e0fd3bd7dea21e9fac589cc32ed7483
oscar <lj20886@bristol.ac.uk>
2023-02-09 10:27:24 +0000

    Fixed Pathfinding graph

--- Treevolution/Assets/Scripts/Pathfinding/PathfindingObstacle.cs
+++ Treevolution/Assets/Scripts/Pathfinding/PathfindingObstacle.cs
@@ -1,38 +1,40 @@
 using System.Collections;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Pathfinding
 {
     //Should be attached to any obstacles which should be avoided during pathfinding
     public class PathfindingObstacle : MonoBehaviour
     {
-        Bounds bounds;
-
         //TODO: move these to central location
         [SerializeField]
         float nodeMargins = 0.1f;
         [SerializeField]
         float floorOffset = 0.1f;
+        Bounds bounds;
+        Collider myCollider;
 
         private void Start()
         {
-            bounds = gameObject.GetComponent<Collider>().bounds;
+            myCollider = this.GetComponent<Collider>();
+            bounds = myCollider.bounds;
             PathfindingGraphGenerator.GetObstacleDataEvent += GetObstacleBoundsEventHandler;
         }
         void GetObstacleBoundsEventHandler(object sender, EventArgs args)
         {
-            float halfWidth = bounds.extents.x + nodeMargins;
-            float halfDepth = bounds.extents.z + nodeMargins;
-            Vector3 floorPosition = gameObject.transform.position - (gameObject.transform.up * (bounds.extents.y - floorOffset));
-            Debug.DrawLine(floorPosition, floorPosition + Vector3.up * 5, Color.blue);
+            float halfWidth = myCollider.transform.localScale.x / 2 + nodeMargins;
+            float halfDepth = myCollider.transform.lossyScale.z / 2 + nodeMargins;
+            Vector3 floorPosition = bounds.center - (myCollider.transform.up * (bounds.extents.y - floorOffset));
+            Debug.DrawLine(bounds.center, bounds.center + halfWidth * myCollider.transform.right, Color.black, 100);
+            Debug.DrawLine(bounds.center, bounds.center + halfDepth * myCollider.transform.forward, Color.black, 100);
             Vector3[] possiblePFPoints = new Vector3[]
             {
-            floorPosition + halfWidth * gameObject.transform.right + halfDepth * gameObject.transform.forward,
-            floorPosition + halfWidth * gameObject.transform.right - halfDepth * gameObject.transform.forward,
-            floorPosition + -halfWidth * gameObject.transform.right + halfDepth * gameObject.transform.forward,
-            floorPosition + -halfWidth * gameObject.transform.right - halfDepth * gameObject.transform.forward
+            floorPosition + halfWidth * myCollider.transform.right + halfDepth * myCollider.transform.forward,
+            floorPosition + halfWidth * myCollider.transform.right - halfDepth * myCollider.transform.forward,
+            floorPosition - halfWidth * myCollider.transform.right + halfDepth * myCollider.transform.forward,
+            floorPosition - halfWidth * myCollider.transform.right - halfDepth * myCollider.transform.forward
             };
             PathfindingGraphGenerator.AddObstacleData(bounds, possiblePFPoints);
         }
