fd5d0cdea62519bb71c52b1e2c21b5b7ee568601
Oscar Hewlett <lj20886@bristol.ac.uk>
2023-02-09 12:42:54 +0000

    Tower Generation working

--- Treevolution/Assets/Scripts/Towers/TowerData.cs
+++ Treevolution/Assets/Scripts/Towers/TowerData.cs
@@ -5,5 +5,21 @@ using UnityEngine;
 //Contains back-end data for the towers
 public class TowerData
 {
-    
+    private int damage { get; }
+    private int range { get; }
+    private int fireSpeed { get; }
+
+    public static int numAttributes = 3; // number of attributes which determine tower behaviour
+    public TowerData(int damage, int range, int fireSpeed)
+    {
+        this.damage = damage;
+        this.range = range;
+        this.fireSpeed = fireSpeed;
+    }
+    public TowerData()
+    {
+        this.damage = 0;
+        this.range = 0;
+        this.fireSpeed = 0;
+    }
 }
