3d1e0620a88ec3f199f15f7ed1ec6464479f9bf2
Oscar Hewlett <lj20886@bristol.ac.uk>
2023-02-09 12:58:31 +0000

    Created TowerGeneration test

--- Treevolution/Assets/Scripts/Towers/TowerData.cs
+++ Treevolution/Assets/Scripts/Towers/TowerData.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 
 //Contains back-end data for the towers
 public class TowerData
 {
-    private int damage { get; }
-    private int range { get; }
-    private int fireSpeed { get; }
+    public int Damage { get; private set; }
+    public int Range { get; private set; }
+    public int FireSpeed { get; private set; }
 
     public static int numAttributes = 3; // number of attributes which determine tower behaviour
     public TowerData(int damage, int range, int fireSpeed)
     {
-        this.damage = damage;
-        this.range = range;
-        this.fireSpeed = fireSpeed;
-    }
-    public TowerData()
-    {
-        this.damage = 0;
-        this.range = 0;
-        this.fireSpeed = 0;
+        this.Damage = damage;
+        this.Range = range;
+        this.FireSpeed = fireSpeed;
     }
 }
