209747a50b3a0027ec0dca6eeb6d45ce14d6e620
Will Tripp <will.tripp@btinternet.com>
2023-02-13 15:07:38 +0000

    merged and integrated trees

---
+++ Treevolution/Assets/TreeScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private int health = 5;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            Destroy(collision.gameObject);
+        health -= 1;
+        if (health == 0) Destroy(gameObject);
+    }
+
+}
