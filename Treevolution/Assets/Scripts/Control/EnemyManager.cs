12bd87682401445d5924232cd1b20f759b27bb10
oscar <lj20886@bristol.ac.uk>
2023-03-07 16:00:35 +0000

    Tree spawning in centre and Enemy spawning on edges

--- Treevolution/Assets/Scripts/Control/EnemyManager.cs
+++ Treevolution/Assets/Scripts/Control/EnemyManager.cs
@@ -12,12 +12,15 @@ public class EnemyManager : MonoBehaviour
     public float spawnRate = 1;
     public float coordinate_X = -5;
     public float coordinate_Y = 5;
+
     float spawnHeight = 0.5f;
     public Vector3 spawnCentre = new Vector3(0, 0, 0);
 
+    private (Vector3 centre, Vector3 vert, Vector3 horz)[] spawnVectors = new (Vector3 centre, Vector3 vert, Vector3 horz)[2];
+
     bool started = false;
     public bool DevMode = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +34,7 @@ public class EnemyManager : MonoBehaviour
         {
             if (timer < spawnRate)
                 timer = timer + Time.deltaTime;
-            else
+            else if (started)
             {
                 spawnEnemy();
                 timer = 0;
@@ -39,19 +42,32 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
-    void spawnEnemy(){
-        Vector3 randomSpawnPosition = spawnCentre + new Vector3(Random.Range(-coordinate_X, coordinate_X), spawnHeight, Random.Range(-coordinate_Y, coordinate_Y));
+    void spawnEnemy()
+    {
+        //Vector3 randomSpawnPosition = spawnCentre + new Vector3(Random.Range(-coordinate_X, coordinate_X), spawnHeight, Random.Range(-coordinate_Y, coordinate_Y));
+
+        int LR = Random.Range(0, 2); // Random
+        (Vector3 centre, Vector3 vert, Vector3 horz) spawnAxes = spawnVectors[LR]; // chooses either the left or right edge to spawn from.
+        float vFraction = Random.value; // vertical fraction
+        float hFraction = Random.value; // horizontal fraction
+
+        Vector3 randomSpawnPosition = spawnAxes.centre + spawnAxes.vert * vFraction + spawnAxes.horz * hFraction;
         _enemies.Add(Instantiate(cubePrefab, randomSpawnPosition, transform.rotation));
     }
 
     public void StartSpawning()
     {
         started = true;
-        Vector3 vertical = GameProperties.TopLeftCorner - GameProperties.BottomLeftCorner;
-        Vector3 horizontal = GameProperties.TopRightCorner - GameProperties.TopLeftCorner;
-        spawnCentre = GameProperties.BottomLeftCorner + (0.5f * vertical) + (0.1f * horizontal);
-        coordinate_X = horizontal.x * 0.04f;
-        coordinate_Y = vertical.z *0.2f;
+        Vector3 verticalLeft = GameProperties.BottomLeftCorner - GameProperties.TopLeftCorner;
+        Vector3 horizontalLeft = (GameProperties.TopRightCorner - GameProperties.TopLeftCorner) * 0.1f;
+        Vector3 verticalRight = GameProperties.BottomRightCorner - GameProperties.TopRightCorner;
+        Vector3 horizontalRight = (GameProperties.TopLeftCorner - GameProperties.TopRightCorner) * 0.1f;
+        spawnVectors[0] = (GameProperties.TopLeftCorner, verticalLeft, horizontalLeft);
+        spawnVectors[1] = (GameProperties.TopRightCorner, verticalRight, horizontalRight);
+        //spawnCentre = GameProperties.BottomLeftCorner + (0.5f * vertical) + (0.1f * horizontal);
+        //coordinate_X = horizontal.x * 0.04f;
+        //coordinate_Y = vertical.z *0.2f;
+
         spawnHeight = 2f + GameProperties.FloorHeight;
     }
 
