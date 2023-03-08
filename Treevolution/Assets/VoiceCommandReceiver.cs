9054d18d2cae69cf9ccadbbb48fbdcd8f6c26fe1
Will Tripp <will.tripp@btinternet.com>
2023-03-08 12:01:51 +0000

    Forgot files

---
+++ Treevolution/Assets/VoiceCommandReceiver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceCommandReceiver : MonoBehaviour
+{
+    public void TestVoiceCommand()
+    {
+        Debug.Log("Test");
+    }
+}
