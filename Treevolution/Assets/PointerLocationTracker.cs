1d201d83e8ef6946cc6c7facab06e980b6eda040
Will Tripp <will.tripp@btinternet.com>
2023-03-08 12:30:07 +0000

    Demo pointer and voice interaction

--- Treevolution/Assets/PointerLocationTracker.cs
+++ Treevolution/Assets/PointerLocationTracker.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
 using UnityEngine;
 
 public class PointerLocationTracker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject pointer;
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(IMixedRealityInputSource s in CoreServices.InputSystem.DetectedInputSources)
+        {
+            if(s.SourceType == InputSourceType.Hand)
+            {
+                foreach(IMixedRealityPointer p in s.Pointers)
+                {
+                    if(!(p is IMixedRealityNearPointer) && p.Result != null)
+                    {
+                        pointer.transform.position =  p.Result.Details.Point;
+                    }
+                }
+            }
+        }
     }
 }
