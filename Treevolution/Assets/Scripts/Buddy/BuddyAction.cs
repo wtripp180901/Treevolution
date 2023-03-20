eb9c95f21f445fc63dbc1186d1b4692c78931996
Will Tripp <will.tripp@btinternet.com>
2023-03-20 15:59:11 +0000

    Set up scene for testing buddy system

---
+++ Treevolution/Assets/Scripts/Buddy/BuddyAction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BUDDY_ACTION_TYPES { Move }
+
+public class BuddyAction
+{
+    public readonly BUDDY_ACTION_TYPES actionType;
+    public readonly Vector3 location;
+
+    public BuddyAction(BUDDY_ACTION_TYPES type,Vector3 location)
+    {
+        this.actionType = type;
+        this.location = location;
+    }
+}
---
+++ Treevolution/Assets/Scripts/Buddy/BuddyScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyScript : MonoBehaviour
+{
+    Queue<BuddyAction> actionQueue = new Queue<BuddyAction>();
+    Rigidbody rig; //This is what physics are applied to in order to move the object
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: If actionQueue is not empty and you are not currently performing an action, start performing the next action
+        //There is only one type of action impemented so far. If the current BuddyAction type is Move, start moving to the location in the BuddyAction
+        //You can get a path to the location by calling Pathfinding.Pathfinder.GetPath(transform.position,location) (store this somewhere). This gives an
+        //ordered list of points you move to in order to get to the location (this avoids walls).
+    }
+
+    //Used for physics updates to move the object
+    private void FixedUpdate()
+    {
+        //You can move the buddy using rig.MovePosition(newPosition)
+        //You can get the current position of the buddy using transform.position, so to move it use rig.MovePosition(transform.position + direction)
+        //When you are close enough to a point (e.g (currentTarget - transform.position).magnitude < 0.05f), start moving to the next point in the path.
+        //If you have visited every point in the path, the action is complete
+    }
+
+    //As a test I am calling this from PhaseTransition - Will
+    public void GiveInstructions(List<BuddyAction> actions)
+    {
+        //Add actions to actionQueue
+    }
+}
