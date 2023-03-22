47c0a9a6669e01c55331ff9e047e0294a3d18d8f
oscar <lj20886@bristol.ac.uk>
2023-03-22 19:15:27 +0000

    Calibration stage needs testing on HL2

--- Treevolution/Assets/Scripts/UI/UIController.cs
+++ Treevolution/Assets/Scripts/UI/UIController.cs
@@ -8,6 +8,11 @@ public class UIController : MonoBehaviour
 {
     public GameObject infoDialogPrefab;
     public GameObject buttonDialogPrefab;
+
+    public Material backPlateGrey;
+    public Material backPlateOrange;
+    public Material backPlateGreen;
+
     public TMP_Text infoText;
     public int roundTime = 60;
     private EnemyManager enemyManager;
@@ -22,13 +27,15 @@ public class UIController : MonoBehaviour
     public void CalibrationPopUp()
     {
         Dialog d = Dialog.Open(infoDialogPrefab, DialogButtonType.None, "Calibrate Game Board", "Find, and look at the QR Code in the corner of the table to calibrate the Game Board.", true);
+        d.gameObject.transform.GetChild(3).gameObject.GetComponent<MeshRenderer>().material = backPlateOrange;
         openDialogs.Add(d);
     }
 
     public void CalibrationSuccessPopUp()
     {
         closeOpenDialogs();
-        Dialog d = Dialog.Open(buttonDialogPrefab, DialogButtonType.Close, "Calibration Success", "You have successfully calibrated the Game Board! Ensure that the board bounds line up with the table.", true);
+        Dialog d = Dialog.Open(buttonDialogPrefab, DialogButtonType.Close, "Calibration Success", "You have successfully calibrated the Game Board! Ensure that it lines up with the table, otherwise adjust.", true);
+        d.gameObject.transform.GetChild(3).gameObject.GetComponent<MeshRenderer>().material = backPlateGreen;
         openDialogs.Add(d);
     }
 
