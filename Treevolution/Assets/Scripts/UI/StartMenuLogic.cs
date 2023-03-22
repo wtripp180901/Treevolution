0558d5d6e085a1adc2359ce321ec3cd82e4284fa
oscar <lj20886@bristol.ac.uk>
2023-03-22 13:39:29 +0000

    Added MainMenu Scene and transition

---
+++ Treevolution/Assets/Scripts/UI/StartMenuLogic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.QR;
+
+public class StartMenuLogic : MonoBehaviour
+{
+    public GameObject SettingsDialog;
+
+    private void Start()
+    {
+        QRCodeWatcher.RequestAccessAsync();
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+        //Dialog.Open(DialogPrefabGuide1, DialogButtonType.OK, "Test Dialog1", "This is Test1", true);
+    }
+
+    public void OpenSettings(GameObject caller)
+    {
+        Dialog d = Dialog.Open(SettingsDialog, DialogButtonType.OK, "Settings", "Lorem Ipsum", true);
+        d.OnClosed = delegate (DialogResult dr) { caller.SetActive(true); };
+
+    }
+}
+
--- Treevolution/Assets/Scripts/UI/UIController.cs
+++ Treevolution/Assets/Scripts/UI/UIController.cs
@@ -34,3 +34,4 @@ public class UIController : MonoBehaviour
         time -= 1;
     }
 }
+
