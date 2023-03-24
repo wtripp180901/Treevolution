a8f7ed7aa5beea8ffeaec275c829cd157f5c4c73
Will Tripp <will.tripp@btinternet.com>
2023-03-24 11:37:01 +0000

    Removed test function call

--- Treevolution/Assets/Scripts/Voice/VoiceCommandReceiver.cs
+++ Treevolution/Assets/Scripts/Voice/VoiceCommandReceiver.cs
@@ -33,7 +33,6 @@ public class VoiceCommandReceiver : MonoBehaviour
 
     public void LightningBolt()
     {
-        ProcessDictation("proceed and dispatch");
         GameObject[] enemies = enemyManager.enemies;
         Vector2 pointerPoint = new Vector2(pointer.transform.position.x, pointer.transform.position.z);
         for (int i = 0;i < enemies.Length; i++)
