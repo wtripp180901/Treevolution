5281305061aae0e5a50898e0d48998442839422f
oscar <lj20886@bristol.ac.uk>
2023-04-01 13:17:16 +0100

    GameStateManager tests

---
+++ Treevolution/Assets/Tests/GameStateManagerTests.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GameStateManagerTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void GameStateManagerStateTransitions()
+    {
+        // Calibration
+        GameStateManager gsm = new GameStateManager(true);
+        GameProperties.SetTestProperties(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.one, Vector3.zero, Pose.identity, 0);
+        Assert.AreEqual(GameStateManager.GameState.Calibration, gsm.currentGameState);
+        
+        // Calibration Success
+        try
+        {
+            gsm.CalibrationSuccess();
+        }
+        catch (System.ArgumentException e)
+        { }
+        Assert.AreEqual(GameStateManager.GameState.Calibration_Success, gsm.currentGameState);
+        
+        // Tutorial Plan
+        try
+        {
+            gsm.BeginTutorialPlan();
+        }
+        catch (System.ArgumentException e)
+        { }
+        Assert.AreEqual(GameStateManager.GameState.Tutorial_Plan, gsm.currentGameState);
+
+        // Tutorial Battle
+        try
+        {
+            gsm.BeginBattle();
+        }
+        catch (System.NullReferenceException e)
+        { }
+        Assert.AreEqual(GameStateManager.GameState.Tutorial_Battle, gsm.currentGameState);
+
+        // Round Plan
+        try
+        {
+            gsm.BeginRound();
+        }
+        catch (System.NullReferenceException e)
+        { }
+        Assert.AreEqual(GameStateManager.GameState.Round_Plan, gsm.currentGameState);
+        
+        // Round Battle
+        try
+        {
+            gsm.BeginBattle();
+        }
+        catch (System.NullReferenceException e)
+        { }
+        Assert.AreEqual(GameStateManager.GameState.Round_Battle, gsm.currentGameState);
+    }
+}
