e74aa35518693f61bc63e84295640fb66a5a01ab
Will Tripp <will.tripp@btinternet.com>
2023-04-07 20:32:25 +0100

    Set up basic dictation tests

--- Treevolution/Assets/Tests/DictationTests.cs
+++ Treevolution/Assets/Tests/DictationTests.cs
@@ -5,39 +5,46 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using Microsoft.MixedReality.Toolkit.Input;
-using Microsoft.MixedReality.Toolkit.Tests;
 
 public class DictationTests
 {
-    [UnitySetUp]
-    public IEnumerator Init()
-    {
-        // in most play mode test cases you would want to at least create an MRTK GameObject using the default profile
-        TestUtilities.InitializeMixedRealityToolkit(true);
-        yield return null;
-    }
-
-    // Destroy the scene - this method is called after each test listed below has completed
-    // Note that this uses UnityTearDown instead of [TearDown] because the init function needs to await a frame passing
-    // to ensure that the MRTK system has fully torn down before the next test setup->run cycle starts.
-    [UnityTearDown]
-    public IEnumerator TearDown()
-    {
-        PlayModeTestUtilities.TearDown();
-        yield return null;
-    }
 
     [UnityTest]
     public IEnumerator testBasicMovementCommand()
     {
+        GameObject pointer;
+        setupBasicMovementScene(out pointer);
+
+        List<BuddyAction> result = null;
+        yield return new LanguageParser(Resources.Load<TextAsset>("basewords").text).GetInstructionStream(new string[]{ "go", "here."}, (x => result = x));
+        Assert.AreEqual(result[0].location, pointer.transform.position);
+    }
+
+    [UnityTest]
+    public IEnumerator BasicMovementWorksWithWordNotInBasewords()
+    {
+        GameObject pointer;
+        setupBasicMovementScene(out pointer);
+
+        List<BuddyAction> result = null;
+        yield return new LanguageParser(Resources.Load<TextAsset>("basewords").text).GetInstructionStream(new string[] { "proceed", "here." }, (x => result = x));
+        Assert.AreEqual(result[0].location, pointer.transform.position);
+    }
+
+    void setupBasicMovementScene(out GameObject pointer)
+    {
         GameObject logic = new GameObject();
-        GameObject pointer = new GameObject();
+        logic.tag = "Logic";
+
+        GameObject moveToMarker = new GameObject();
+        moveToMarker.tag = "MoveToMarker";
+
+        pointer = new GameObject();
         pointer.transform.position = new Vector3(0, 0, 0);
         PointerLocationTracker pointerTracker = logic.AddComponent<PointerLocationTracker>();
         pointerTracker.pointer = pointer;
-        List<BuddyAction> result = null;
-        yield return new LanguageParser(Resources.Load<TextAsset>("basewords").text).GetInstructionStream(new string[]{ "go", "here."}, (x => result = x));
-        Assert.AreEqual(result.Count, 1);
+        pointerTracker.StartSampling();
+        pointerTracker.FinishSampling();
     }
 
 }
