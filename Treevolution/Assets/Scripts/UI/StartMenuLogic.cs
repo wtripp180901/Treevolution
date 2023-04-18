32df020c6e827a665211ea95fbb912369433d00a
oscar <lj20886@bristol.ac.uk>
2023-04-18 12:44:10 +0100

    Added Planning Music and updated settings menu

--- Treevolution/Assets/Scripts/UI/StartMenuLogic.cs
+++ Treevolution/Assets/Scripts/UI/StartMenuLogic.cs
@@ -1,5 +1,6 @@
 using Microsoft.MixedReality.QR;
 using Microsoft.MixedReality.Toolkit.UI;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,16 +18,48 @@ public class StartMenuLogic : MonoBehaviour
     /// </summary>
     public GameObject StartMenu;
 
+    private TouchScreenKeyboard _keyboard;
+    private string _keyboardText = "";
+
+    private int widthOrDepth = -1; // 0 = editing width, 1 = editing depth
+
+    private int _tableWidthMM = 240;
+    private int _tableDepthMM = 160;
 
     private void Start()
     {
         QRCodeWatcher.RequestAccessAsync();
     }
 
+    private void Update()
+    {
+        if(_keyboard != null && _keyboard.status != TouchScreenKeyboard.Status.Visible)
+        {
+            _keyboardText = _keyboard.text;
+            if (widthOrDepth == 0)
+            {
+                try
+                {
+                    _tableWidthMM = int.Parse(_keyboardText);
+                }
+                catch { }
+            }
+            else if (widthOrDepth == 1)
+            {
+                try
+                {
+                    _tableDepthMM = int.Parse(_keyboardText);
+                }
+                catch { }
+            }
+            widthOrDepth = -1;
+        }
+    }
+
     /// <summary>
     /// Opens the start menu.
     /// </summary>
-    public void openStartMenu()
+    public void OpenStartMenu()
     {
         StartMenu.SetActive(true);
     }
@@ -45,8 +78,23 @@ public class StartMenuLogic : MonoBehaviour
     /// <param name="caller"></param>
     public void OpenSettings()
     {
-        Dialog d = Dialog.Open(SettingsDialogPrefab, DialogButtonType.Close, "Settings", "There are currently no settings available... Sorry :(", true);
-        d.OnClosed = delegate (DialogResult dr) { openStartMenu(); };
+        SettingsDialogPrefab.SetActive(true);
     }
+
+
+    public void EditTableWidth()
+    {
+        widthOrDepth = 0;
+        _keyboard = TouchScreenKeyboard.Open(_tableWidthMM.ToString(), TouchScreenKeyboardType.NumberPad, false, false, false ,false, _tableWidthMM.ToString());
+    }
+
+
+    public void EditTableDepth()
+    {
+        widthOrDepth = 1;
+        _keyboard = TouchScreenKeyboard.Open(_tableDepthMM.ToString(), TouchScreenKeyboardType.NumberPad, false, false, false, false, _tableDepthMM.ToString());
+
+    }
+    //Settings & Credits", "Click OK to change the table dimensions, or Close to go back.\nGame made by Stefan Corneci, Mingtong Du, Oscar Hewlett, Aristotelis Kostelenos, William Tripp, Kuncheng Xiao, Ash Zhuang.\nMusic by Hadi Rahmani and Billy Crook.
 }
 
